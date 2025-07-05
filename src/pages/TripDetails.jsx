f4d782068cca14b848c69765a0b74bcd6decb613
Charan27c6 <22pa1a05c6@vishnu.edu.in>
2025-07-05 22:22:37 +0530

    Updated TripDetails.jsx

--- src/pages/TripDetails.jsx
+++ src/pages/TripDetails.jsx
@@ -22,8 +22,8 @@ import {
   Share
 } from 'lucide-react'
 import { useTheme } from '../contexts/ThemeContext'
-import Navbar from './Navbar'
-import BottomNavbar from './BottomNavbar'
+import Navbar from '../components/Navbar'
+import BottomNavbar from '../components/BottomNavbar'
 
 const TripDetails = () => {
   const navigate = useNavigate()
